ea9b9a165a5ab6efb0e3aef80caceac3bf1b09ab
Calvin Li <calvinliqh@gmail.com>
2019-03-02 17:16:42 -0800

    switching to OCR

--- strcmp.py
+++ strcmp.py
@@ -1,6 +1,6 @@
 import ImageMaker
 import Matcher
-
+import Ocr
 
 threshold = 0.9
 
@@ -9,8 +9,6 @@ def strcmp(candidate, search_text, search_image, temp_folder):
     # create image of string
     text_img = ImageMaker.text_to_image(candidate, temp_folder, x_padding=10)
 
-    # attempt template matching
-    results, score = Matcher.match(search_text, text_img, search_image)
-
-    # process results?
-    return score >= threshold
+    # attempt OCR
+    Ocr.ocr(search_text, search_image)
+    return
